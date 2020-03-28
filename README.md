8365c8e956bd8f8166cd4fd225eefc2194be90bf
telec <>
2020-03-28 01:50:50 +0100

    Init repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple TP - Bukkit
+
+You don't want your players to abuse the `/tp` command, and you don't want to spend time configuring a plugin ? SimpleTP !
+
+## How ?
+
+1. Code the plugin
---
+++ src/fr/telec/simpleTP/SimpleTP.java
@@ -0,0 +1,32 @@
+package fr.telec.simpleTP;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleTP extends JavaPlugin implements Listener {
+
+
+	/*
+	 * Plugin setup
+	 */
+
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("update")) {
+			return true;
+		}
+		return false;
+	}
+
+}
